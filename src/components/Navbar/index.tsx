92f2170ee818611e07913c0bc9c406a827e1c9ca
Qasim Fida <qasimfida0054@gmail.com>
2023-04-06 22:18:10 +0500

    Added internationalization

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
+import { useTranslation } from 'next-i18next'
 import Button from '../Button';
 import { LanguageIcon, UserIcon } from '../Icons';
+import { useRouter } from 'next/router';
+import { cn } from '@/helpers/cn';
 
 interface IProps {
   className: string;
 }
 
 const Navbar = ({ className }: IProps) => {
+  const { t } = useTranslation();
+  const router  = useRouter();
+  const handleLanguageChange = () => {
+    router.push(router.pathname, router.asPath, { locale: router.locale === 'ar' ? 'en': 'ar'  })
+  }
   return (
     <header
-      className={`py-[10px] px-[25px] flex justify-between items-center bg-full-dark ${className}`}
+      className={cn`py-[10px] px-[25px] flex justify-between items-center bg-full-dark ${className}`}
     >
       <Button className="">Ash Shara'i</Button>
       <div className="flex items-center">
-        <a href="#" className="flex items-center text-white mr-4">
+        <button className="flex items-center mr-4 text-white outline-none" onClick={handleLanguageChange}>
           <LanguageIcon className="mr-2" />
-          عربي
-        </a>
+          {t('name')}
+        </button>
         <a href="#" className="flex items-center text-white">
           <UserIcon className="mr-2" />
           Login
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,7 +1,6 @@
 import Carousel from '@/components/Carousel';
 import Collapse from '@/components/Collapse';
 import Hero from '@/components/Hero';
-import { MenuContext } from '@/contexts/MenuContext';
 import React, { useContext, useState } from 'react';
 
 const menus = [
@@ -24,13 +23,12 @@ const menus = [
 
 const HomeContainer = () => {
   const [openIndex, setOpenIndex] = useState(1);
-  const { state, setState } = useContext(MenuContext);
 
   const handleOpen = (index: number) => {
     setOpenIndex(index);
   };
   return (
-    <div className="bg-full-dark min-h-screen px-8">
+    <div className="min-h-screen px-8 bg-full-dark">
       <Hero className="" />
       {menus.map((menu, index) => (
         <Collapse
